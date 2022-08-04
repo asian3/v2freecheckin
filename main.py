45388eb8512f6accad6132eae58c8619f0057543
asian3 <61188795+asian3@users.noreply.github.com>
2022-08-04 22:32:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -81,8 +81,6 @@ if __name__ == "__main__":
     parser.add_argument('--semail', type=str, help='发送邮箱')
     parser.add_argument('--secode', type=str, help='发送密码')
     args = parser.parse_args()
-    a=[args.secode,args.semail]
-    print(a)
     msg = main(args.username,args.password)
     print(msg)
     send(info=msg,mail=[args.semail,args.secode],receivers=args.remail)
